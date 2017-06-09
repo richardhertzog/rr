84226970994eb3a5f9efbd448ceba2c5ed6349ee
Richard Hertzog <richard.hertzog@gmail.com>
2017-06-09 00:35:16 -0700

    formatting

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -1,40 +1,39 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import App from 'grommet/components/App';
-import Split from 'grommet/components/Split';
+import App from 'grommet/components/App'
+import Split from 'grommet/components/Split'
 
-import NavSidebar from './NavSidebar';
-import { navResponsive } from '../actions/nav';
+import NavSidebar from './NavSidebar'
+import { navResponsive } from '../actions/nav'
 
-import Login from '../screens/Login';
-import Dashboard from '../screens/Dashboard';
-import Tasks from '../screens/Tasks';
-import Task from '../screens/Task';
-import NotFound from '../screens/NotFound';
+import Login from '../screens/Login'
+import Dashboard from '../screens/Dashboard'
+import Tasks from '../screens/Tasks'
+import Task from '../screens/Task'
+import NotFound from '../screens/NotFound'
 
 class Main extends Component {
-
-  constructor() {
-    super();
-    this._onResponsive = this._onResponsive.bind(this);
+  constructor () {
+    super()
+    this._onResponsive = this._onResponsive.bind(this)
   }
 
-  _onResponsive(responsive) {
-    this.props.dispatch(navResponsive(responsive));
+  _onResponsive (responsive) {
+    this.props.dispatch(navResponsive(responsive))
   }
 
-  render() {
+  render () {
     const {
       nav: { active: navActive, enabled: navEnabled, responsive }
-    } = this.props;
-    const includeNav = (navActive && navEnabled);
-    let nav;
+    } = this.props
+    const includeNav = (navActive && navEnabled)
+    let nav
     if (includeNav) {
-      nav = <NavSidebar />;
+      nav = <NavSidebar />
     }
-    const priority = (includeNav && responsive === 'single' ? 'left' : 'right');
+    const priority = (includeNav && responsive === 'single' ? 'left' : 'right')
 
     return (
       <App centered={false}>
@@ -43,7 +42,7 @@ class Main extends Component {
             onResponsive={this._onResponsive}>
             {nav}
             <Switch>
-              <Route exact={true} path='/' component={Dashboard} />
+              <Route exact path='/' component={Dashboard} />
               <Route path='/dashboard' component={Dashboard} />
               <Route path='/login' component={Login} />
               <Route path='/tasks/:id' component={Task} />
@@ -53,7 +52,7 @@ class Main extends Component {
           </Split>
         </Router>
       </App>
-    );
+    )
   }
 }
 
@@ -64,10 +63,10 @@ Main.propTypes = {
     enabled: PropTypes.bool,
     responsive: PropTypes.string
   })
-};
+}
 
 const select = state => ({
   nav: state.nav
-});
+})
 
-export default connect(select)(Main);
+export default connect(select)(Main)
--- src/js/components/NavSidebar.js
+++ src/js/components/NavSidebar.js
@@ -1,57 +1,55 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 
-import Sidebar from 'grommet/components/Sidebar';
-import Header from 'grommet/components/Header';
-import Footer from 'grommet/components/Footer';
-import Title from 'grommet/components/Title';
-import Menu from 'grommet/components/Menu';
-import Button from 'grommet/components/Button';
-import CloseIcon from 'grommet/components/icons/base/Close';
-import Logo from 'grommet/components/icons/Grommet';
-import Anchor from 'grommet/components/Anchor';
+import Sidebar from 'grommet/components/Sidebar'
+import Header from 'grommet/components/Header'
+import Footer from 'grommet/components/Footer'
+import Title from 'grommet/components/Title'
+import Menu from 'grommet/components/Menu'
+import Button from 'grommet/components/Button'
+import CloseIcon from 'grommet/components/icons/base/Close'
+import Logo from 'grommet/components/icons/Grommet'
+import Anchor from 'grommet/components/Anchor'
 
-import SessionMenu from './SessionMenu';
-import { navActivate } from '../actions/nav';
+import SessionMenu from './SessionMenu'
+import { navActivate } from '../actions/nav'
 
 class NavSidebar extends Component {
-
-  constructor() {
-    super();
-    this._onClose = this._onClose.bind(this);
+  constructor () {
+    super()
+    this._onClose = this._onClose.bind(this)
   }
 
-  _onClose() {
-    this.props.dispatch(navActivate(false));
+  _onClose () {
+    this.props.dispatch(navActivate(false))
   }
 
-  render() {
-    const { nav: { items } } = this.props;
+  render () {
+    const { nav: { items } } = this.props
 
     const links = items.map(page => (
       <Anchor key={page.label} path={page.path} label={page.label} />
-    ));
+    ))
 
     return (
-      <Sidebar colorIndex='neutral-3' fixed={true}>
+      <Sidebar colorIndex='neutral-3' fixed>
         <Header size='large' justify='between' pad={{ horizontal: 'medium' }}>
           <Title onClick={this._onClose} a11yTitle='Close Menu'>
             <Logo />
             <span>Grommet</span>
           </Title>
-          <Button icon={<CloseIcon />} onClick={this._onClose} plain={true}
+          <Button icon={<CloseIcon />} onClick={this._onClose} plain
             a11yTitle='Close Menu' />
         </Header>
-        <Menu fill={true} primary={true}>
+        <Menu fill primary>
           {links}
         </Menu>
         <Footer pad={{ horizontal: 'medium', vertical: 'small' }}>
           <SessionMenu dropAlign={{ bottom: 'bottom' }} />
         </Footer>
       </Sidebar>
-    );
+    )
   }
-
 }
 
 NavSidebar.propTypes = {
@@ -62,10 +60,10 @@ NavSidebar.propTypes = {
       label: PropTypes.string
     }))
   })
-};
+}
 
 const select = state => ({
   nav: state.nav
-});
+})
 
-export default connect(select)(NavSidebar);
+export default connect(select)(NavSidebar)
